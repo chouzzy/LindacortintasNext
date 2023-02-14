6e02668fd2a6da64f66084a00a720c828dc2cee1
Matheus <mathfernando@hotmail.com>
2023-02-14 09:35:58 -0300

    Update WhatsButton.tsx

--- src/components/WhatsButton.tsx
+++ src/components/WhatsButton.tsx
@@ -4,8 +4,22 @@ import { RiWhatsappFill } from "react-icons/ri";
 
 
 export default function WhatsButton() {
+    
+   const [whatsNumber, setWhatsNumber] = useState('')
+   useEffect(() => {
+      const fetchWhatsNumber = async () => {
+         await fetch('/api/random', {method:'get'})
+         .then(response => response.json())
+         .then(data => {
+            setWhatsNumber(data.number)
+            console.log(whatsNumber)
+         })
+      }
+      fetchWhatsNumber()
+   }, [])
+    
     return (
-        <Link className="zap-tag" href="https://wa.me/5516997093759?text=Olá, tudo bem? Acessei o site da lindacor e gostaria de falar com um atendente." target="_blank">
+        <Link className="zap-tag" href=`https://wa.me/${whatsNumber}?text=Olá, tudo bem? Acessei o site da lindacor e gostaria de falar com um atendente." target="_blank`>
         
         <Flex zIndex={1} bg='#25D366' color="#fffafa"
         fontSize='1.8rem' borderRadius={'full'} p={4} 
