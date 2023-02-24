cd43e444391829f55786ea2123a5c1c9b81f6090
Matheus <mathfernando@hotmail.com>
2023-02-24 10:31:02 -0300

    correção no random

--- src/pages/api/random.tsx
+++ src/pages/api/random.tsx
@@ -2,7 +2,7 @@
 export default async (req,res) => {
    let numbers = ['5516991439381','5516997093759','5516996092935','5516997092489','5516997097415']
 //    let numbers = ['5516996092936','5516997093759','5516996092935','5516991439381']
-   let randomIndex = Math.floor(Math.random() * 4)
+   let randomIndex = Math.floor(Math.random() * 5)
    let whatsNumber = numbers[randomIndex]
    res.status(200).json({number: `${whatsNumber}`})
 }
