7ffbf24703bd29f11702ad0df60d5ba3940133c5
mathf <Mathias>
2023-05-31 10:12:01 -0300

    push number 2489

--- src/pages/api/random.tsx
+++ src/pages/api/random.tsx
@@ -1,8 +1,8 @@
 
 export default async (req,res) => {
-   let numbers = ['5516991439381','5516997093759']
+   let numbers = ['5516991439381','5516997093759', '5516997092489']
 //    let numbers = ['5516996092936','5516997093759','5516996092935','5516991439381']
-   let randomIndex = Math.floor(Math.random() * 4)
+   let randomIndex = Math.floor(Math.random() * 3)
    let whatsNumber = numbers[randomIndex]
    res.status(200).json({number: `${whatsNumber}`})
 }
