3f2cfbcd04f9087861aa203107b103166f03ae5b
mathf <Mathias>
2023-07-06 10:53:37 -0300

    correção no botão de whatsapp

--- src/pages/api/random.tsx
+++ src/pages/api/random.tsx
@@ -1,8 +1,7 @@
 
 export default async (req,res) => {
-   let numbers = ['5516991439381']
-//    let numbers = ['5516996092936','5516997093759','5516996092935','5516991439381']
-   let randomIndex = Math.floor(Math.random() * 1)
+   let numbers = ['5516997093759','5516997092489','5516991439381']
+   let randomIndex = Math.floor(Math.random() * 3)
    let whatsNumber = numbers[randomIndex]
    res.status(200).json({number: `${whatsNumber}`})
 }
