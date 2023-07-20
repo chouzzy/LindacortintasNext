387f50a724fe75ed25f5ed7ef9bb1d82d564cabf
mathf <Mathias>
2023-07-20 19:37:44 -0300

    upload do whatsapp do adilson

--- src/pages/api/random.tsx
+++ src/pages/api/random.tsx
@@ -1,7 +1,7 @@
 
 export default async (req,res) => {
-   let numbers = ['5516997093759','5516997092489','5516991439381']
-   let randomIndex = Math.floor(Math.random() * 3)
+   let numbers = ['5516997093759','5516997092489','5516991439381', '5516997097415']
+   let randomIndex = Math.floor(Math.random() * 4)
    let whatsNumber = numbers[randomIndex]
    res.status(200).json({number: `${whatsNumber}`})
 }
