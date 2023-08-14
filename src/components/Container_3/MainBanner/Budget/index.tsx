45a56a9a8d2438b16c6fc2471a8c7c1f5cb58aad
mathf <Mathias>
2023-08-14 15:28:07 -0300

    retirada de whatsapp e add johnny

--- src/components/Container_3/MainBanner/Budget/index.tsx
+++ src/components/Container_3/MainBanner/Budget/index.tsx
@@ -8,19 +8,19 @@ const divMaps = {
       telefone:'(16) 3637-2108',
       telefoneNumber:'tel:+551636372108',
       whatsapp_1:'(16) 99709-7415',
-      whatsapp_2:'(16) 99709-3759',
+      whatsapp_2:'0',
       whatsapp_1_number:'5516997097415',
-      whatsapp_2_number:'5516997093759'
+      whatsapp_2_number:'0'
    },
    store_2:{
       address:"🚩 LOJA 2: Av. Caramuru, 2055 - Jardim Sumare",
       source:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3719.8928401992703!2d-47.824566185441675!3d-21.19641548440527!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94b9bec08620d2d5%3A0x6fa495639a52a700!2sAv.%20Caramuru%2C%202055%20-%202%20-%20Jardim%20Sumare%2C%20Ribeir%C3%A3o%20Preto%20-%20SP%2C%2014025-303!5e0!3m2!1spt-BR!2sbr!4v1648817586786!5m2!1spt-BR!2sbr",
       telefone:'(16) 3621-2108',
       telefoneNumber:'tel:+551636212108',
-      whatsapp_1:'0',
+      whatsapp_1:'(16) 99709-2489',
       whatsapp_2:'(16) 99143-9381',
       whatsapp_3:'0',
-      whatsapp_1_number:'0',
+      whatsapp_1_number:'5516997092489',
       whatsapp_2_number:'5516991439381',
       whatsapp_3_number:'0'
    }
