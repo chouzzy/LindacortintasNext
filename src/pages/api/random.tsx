e6e8f571eca0976b730dafceb243c951d3369236
mathf <Mathias>
2023-09-29 12:50:41 -0300

    remoção do 2489

--- src/pages/api/random.tsx
+++ src/pages/api/random.tsx
@@ -1,6 +1,6 @@
 
 export default async (req,res) => {
-   let numbers = ['5516991439381', '5516997097415','5516997092489','5516997093759']
+   let numbers = ['5516991439381', '5516997097415']
    let randomIndex = Math.floor(Math.random() * 4)
    let whatsNumber = numbers[randomIndex]
    res.status(200).json({number: `${whatsNumber}`})
