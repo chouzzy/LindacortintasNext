2cbb80b1f775b54291e60526f7e3e8bb855f1622
mathf <Mathias>
2023-10-26 13:19:52 -0300

    retirada do 7415 e add 2489

--- src/pages/api/random.tsx
+++ src/pages/api/random.tsx
@@ -1,6 +1,6 @@
 
 export default async (req,res) => {
-   let numbers = ['5516991439381', '5516997097415']
+   let numbers = ['5516991439381', '5516997092489']
    let randomIndex = Math.floor(Math.random() * 2)
    let whatsNumber = numbers[randomIndex]
    res.status(200).json({number: `${whatsNumber}`})
