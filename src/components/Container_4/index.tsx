0fc11bd1accf1651ee74d4c7cf5d5fc2e8558e6b
chouzzy <mathfernando@hotmail.com>
2024-02-20 17:58:07 -0300

    lottie update and remoção do número 2489

--- src/components/Container_4/index.tsx
+++ src/components/Container_4/index.tsx
@@ -1,18 +1,20 @@
 import { Box, Button, Flex, Grid, GridItem, Heading, Image, Link, Text, useBreakpointValue, VStack } from "@chakra-ui/react";
 import animationData from '../../../public/animations/painter.json';
-import Lottie from 'react-lottie'
+import dynamic from "next/dynamic";
+
 import { useState, useEffect } from "react";
 
-export function Container_4() { 
+export function Container_4() {
+
    const [whatsNumber, setWhatsNumber] = useState('')
    useEffect(() => {
       const fetchWhatsNumber = async () => {
-         await fetch('/api/random', {method:'get'})
-         .then(response => response.json())
-         .then(data => {
-            setWhatsNumber(data.number)
-            console.log(whatsNumber)
-         })
+         await fetch('/api/random', { method: 'get' })
+            .then(response => response.json())
+            .then(data => {
+               setWhatsNumber(data.number)
+               console.log(whatsNumber)
+            })
       }
       fetchWhatsNumber()
    }, [])
@@ -20,41 +22,41 @@ export function Container_4() {
    const [telefoneNumber, setTelefoneNumber] = useState('')
    useEffect(() => {
       const fetchWhatsNumber = async () => {
-         await fetch('/api/randomTelefone', {method:'get'})
-         .then(response => response.json())
-         .then(data => {
-            setTelefoneNumber(data.number)
-         })
+         await fetch('/api/randomTelefone', { method: 'get' })
+            .then(response => response.json())
+            .then(data => {
+               setTelefoneNumber(data.number)
+            })
       }
       fetchWhatsNumber()
    }, [])
 
    let socialMediaLinks = {
-      'instagram':'https://www.instagram.com/lindacortintas/',
-      'facebook':'https://www.facebook.com/Lindacor-Tintas-104622232176385',
-      'linkedin':'https://www.linkedin.com/company/lindacor-tintas/',
-      'whatsapp':`https://wa.me/${whatsNumber}?text=Olá, tudo bem? Acessei o site da Lindacor e gostaria de falar com um atendente.`,
-      'telefone':`tel:+${telefoneNumber}`
+      'instagram': 'https://www.instagram.com/lindacortintas/',
+      'facebook': 'https://www.facebook.com/Lindacor-Tintas-104622232176385',
+      'linkedin': 'https://www.linkedin.com/company/lindacor-tintas/',
+      'whatsapp': `https://wa.me/${whatsNumber}?text=Olá, tudo bem? Acessei o site da Lindacor e gostaria de falar com um atendente.`,
+      'telefone': `tel:+${telefoneNumber}`
    }
    const defaultOptions = {
       loop: true,
       autoplay: true,
       animationData: animationData,
       rendererSettings: {
-        preserveAspectRatio: "xMidYMid slice"
+         preserveAspectRatio: "xMidYMid slice"
       }
-    }
+   }
 
    const quemSomos = {
-      title_1:'Quem somos',
-      about_1:'A Lindacor Tintas foi fundada em 1996, em Ribeirão Preto, com a missão de atender com excelência e cuidado os clientes da região. O foco sempre foi o atendimento e qualidade dos produtos, o que proporcionou a inauguração da segunda loja na cidade, em 2009. A capacitação das equipes é prioridade e constante, tal qual a parceria com fornecedores, que desenvolvem linhas de produtos com as mais novas tecnologias.',
-      title_2:'A Lindacor Tintas é associada ao grupo Coopertintas',
-      about_2:'A Coopertintas, foi fundada no dia 30 de maio de 2012, é a união de empresas de varejo do segmento de tintas de Ribeirão Preto e região. Possui como foco estratégico: unir para poder crescer, aumento de competitividade e excelência em qualidade. A Coopertintas tem sua linha própria de produtos.',
+      title_1: 'Quem somos',
+      about_1: 'A Lindacor Tintas foi fundada em 1996, em Ribeirão Preto, com a missão de atender com excelência e cuidado os clientes da região. O foco sempre foi o atendimento e qualidade dos produtos, o que proporcionou a inauguração da segunda loja na cidade, em 2009. A capacitação das equipes é prioridade e constante, tal qual a parceria com fornecedores, que desenvolvem linhas de produtos com as mais novas tecnologias.',
+      title_2: 'A Lindacor Tintas é associada ao grupo Coopertintas',
+      about_2: 'A Coopertintas, foi fundada no dia 30 de maio de 2012, é a união de empresas de varejo do segmento de tintas de Ribeirão Preto e região. Possui como foco estratégico: unir para poder crescer, aumento de competitividade e excelência em qualidade. A Coopertintas tem sua linha própria de produtos.',
    }
 
    const animationSize = useBreakpointValue({
-      base: 240, 
-      sm: 240, 
+      base: 240,
+      sm: 240,
       md: 240,
       lg: 360,
       xl: 360
@@ -62,48 +64,57 @@ export function Container_4() {
 
    return (
       <>
-      
-      <Grid
-      id='#quemSomos'
-      w='100%'
-      fontFamily='Heebo'
-      bgImage='static/img/tinta-2c.png'
-      bgRepeat='repeat-x'
-      justifyContent='center'
-      templateColumns={['repeat(1, 1fr)','repeat(1, 1fr)','repeat(2, 1fr)','repeat(2, 1fr)' ]}
-      pt={8}
-      >
-         <Flex w='100%' my='auto'> 
-            <Lottie
-               options={defaultOptions}
-               height={animationSize}
-               width={animationSize}
-            />
-         </Flex>
-         
-         <Flex minW='60vw'> 
-            <VStack color='gray.600' gap={4} p={6} >
-               <Image src='static/img/lindacor-logo-2.png' maxW={260} my={4} alt='Logo da Lindacor Tintas'/>
-               <Heading fontSize={['1.5rem','1.4rem','1.4rem','1.4rem']} fontWeight='500' textAlign='center'>{quemSomos.title_1}</Heading>
-               <Text fontWeight='400'>{quemSomos.about_1}</Text>
-               <Heading fontSize={['1.5rem','1.4rem','1.4rem','1.4rem']} fontWeight='500' textAlign='center'>{quemSomos.title_2}</Heading>
-               <Text fontWeight='400'>{quemSomos.about_2}</Text>
-            </VStack>
-         </Flex>
-      </Grid>
-      <Flex w='100%' justify='center' textAlign='center' flexDir={['column','column','row', 'row']} gap={2} my={12} >
-         <Link className="liga-tag" href={socialMediaLinks.telefone} _hover={{textDecoration: 'none'}}>
-            <Button className='liga-tag' w={64} p={8} fontWeight='400' fontSize='1.4rem' bgColor='#ffc107' color='gray.50'>
-               LIGUE AGORA!
-            </Button>
-         </Link>
 
-         <Link className='zap-tag' href={socialMediaLinks.whatsapp}  _hover={{textDecoration: 'none'}}>
-            <Button className='zap-tag' w={64} p={8} fontWeight='400' fontSize='1.4rem' bgColor='green.500' color='gray.50'>
-               WHATSAPP
-            </Button>
-         </Link>
-      </Flex>
+         <Grid
+            id='#quemSomos'
+            w='100%'
+            fontFamily='Heebo'
+            bgImage='static/img/tinta-2c.png'
+            bgRepeat='repeat-x'
+            justifyContent='center'
+            templateColumns={['repeat(1, 1fr)', 'repeat(1, 1fr)', 'repeat(2, 1fr)', 'repeat(2, 1fr)']}
+            pt={8}
+         >
+            <Flex w='100%' my='auto'>
+               <Flex mx='auto'maxW={['240px','240px','360px','360px','360px']}>
+
+                  <PlayerWithNoSSR
+                     autoplay
+                     keepLastFrame
+                     loop
+                     src={animationData}
+                  />
+               </Flex>
+            </Flex>
+
+            <Flex minW='60vw'>
+               <VStack color='gray.600' gap={4} p={6} >
+                  <Image src='static/img/lindacor-logo-2.png' maxW={260} my={4} alt='Logo da Lindacor Tintas' />
+                  <Heading fontSize={['1.5rem', '1.4rem', '1.4rem', '1.4rem']} fontWeight='500' textAlign='center'>{quemSomos.title_1}</Heading>
+                  <Text fontWeight='400'>{quemSomos.about_1}</Text>
+                  <Heading fontSize={['1.5rem', '1.4rem', '1.4rem', '1.4rem']} fontWeight='500' textAlign='center'>{quemSomos.title_2}</Heading>
+                  <Text fontWeight='400'>{quemSomos.about_2}</Text>
+               </VStack>
+            </Flex>
+         </Grid>
+         <Flex w='100%' justify='center' textAlign='center' flexDir={['column', 'column', 'row', 'row']} gap={2} my={12} >
+            <Link className="liga-tag" href={socialMediaLinks.telefone} _hover={{ textDecoration: 'none' }}>
+               <Button className='liga-tag' w={64} p={8} fontWeight='400' fontSize='1.4rem' bgColor='#ffc107' color='gray.50'>
+                  LIGUE AGORA!
+               </Button>
+            </Link>
+
+            <Link className='zap-tag' href={socialMediaLinks.whatsapp} _hover={{ textDecoration: 'none' }}>
+               <Button className='zap-tag' w={64} p={8} fontWeight='400' fontSize='1.4rem' bgColor='green.500' color='gray.50'>
+                  WHATSAPP
+               </Button>
+            </Link>
+         </Flex>
       </>
    )
 }
+
+const PlayerWithNoSSR = dynamic(
+   () => import('@lottiefiles/react-lottie-player').then(module => module.Player),
+   { ssr: false },
+);
