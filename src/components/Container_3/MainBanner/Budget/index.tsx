08c21b8c70da305152efcdbe360868e2ebf7918b
chouzzy <mathfernando@hotmail.com>
2024-05-20 11:03:37 -0300

    retirada do 9381 e repondo 2935 v2

--- src/components/Container_3/MainBanner/Budget/index.tsx
+++ src/components/Container_3/MainBanner/Budget/index.tsx
@@ -1,12 +1,13 @@
 import { Grid, GridItem, Heading } from "@chakra-ui/react";
 import { Maps } from "./Maps";
 
+
 const divMaps = {
    store_1:{
       address:"🚩 LOJA 1: R. Dr. João Guião, 1056 - Vila Virginia",
       source:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3720.1360076815504!2d-47.829504085064634!3d-21.186755385913038!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94b9be9217877245%3A0xe17f0f64c1fbd07e!2sR.%20Dr.%20Jo%C3%A3o%20Gui%C3%A3o%2C%201056%20-%20Vila%20Virginia%2C%20Ribeir%C3%A3o%20Preto%20-%20SP%2C%2014030-440!5e0!3m2!1spt-BR!2sbr!4v1648817469914!5m2!1spt-BR!2sbr",
-      telefone:'0',
-      telefoneNumber:'0',
+      telefone:'(16) 3637-2108',
+      telefoneNumber:'tel:+551636372108',
       whatsapp_1:'',
       whatsapp_2:'(16) 99709-7415',
       whatsapp_1_number:'',
@@ -18,10 +19,10 @@ const divMaps = {
       telefone:'(16) 3621-2108',
       telefoneNumber:'tel:+551636212108',
       whatsapp_1:'(16) 99709-2489',
-      whatsapp_2:'(16) 99143-9381',
+      whatsapp_2:'(16) 99609-2935',
       whatsapp_3:'0',
       whatsapp_1_number:'5516997092489',
-      whatsapp_2_number:'5516991439381',
+      whatsapp_2_number:'5516996092935',
       whatsapp_3_number:'0'
    }
 }
