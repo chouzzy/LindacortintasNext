3dc23c3bcae139b8c2571907fa9a455667039b80
chouzzy <mathfernando@hotmail.com>
2024-06-03 18:22:29 -0300

    volta do 7415

--- src/pages/api/random.tsx
+++ src/pages/api/random.tsx
@@ -1,7 +1,7 @@
 
 export default async (req,res) => {
-   let numbers = ['5516996092935','5516997092489']
-   let randomIndex = Math.floor(Math.random() * 1)
+   let numbers = ['5516996092935','5516997092489','5516997097415']
+   let randomIndex = Math.floor(Math.random() * 3)
    let whatsNumber = numbers[randomIndex]
    res.status(200).json({number: `${whatsNumber}`})
 }
