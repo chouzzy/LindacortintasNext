fd779d8fa8a3d96a33aaf233b6da6f0479c82bb8
chouzzy <mathfernando@hotmail.com>
2025-01-28 12:15:24 -0300

    retirada do 7415

--- src/pages/api/random.tsx
+++ src/pages/api/random.tsx
@@ -1,7 +1,7 @@
 
 export default async (req,res) => {
-   let numbers = ['5516996092935','5516997097415','5516991439381']
-   let randomIndex = Math.floor(Math.random() * 3)
+   let numbers = ['5516996092935','5516991439381']
+   let randomIndex = Math.floor(Math.random() * 2)
    let whatsNumber = numbers[randomIndex]
    res.status(200).json({number: `${whatsNumber}`})
 }
